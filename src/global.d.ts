b4ab897ee7b3b63d09601487043adec4cef4d71c
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2022-11-22 13:46:41 +0100

    Got initial game logic working
    No won/lost logic tho

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+export {}
+
+// until ts 5.0
+declare global {
+  interface Array<T> {
+    findLastIndex(
+      predicate: (value: T, index: number, obj: T[]) => unknown,
+      thisArg?: any
+    ): number
+  }
+}
