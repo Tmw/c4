a67d7f95958ad30fb59b93dfd4e456310fdd33e2
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2022-11-23 22:18:07 +0100

    Got prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import React, { useState, useCallback, useEffect } from "react";
-import './app.css';
+import React, { useState, useCallback } from "react";
+import "./app.css";
 
 const BOARD_WIDTH = 7;
 const BOARD_HEIGHT = 6;
 namespace State {
-  export type Player = 'red' | 'yellow'
-  export type Cell = 'open' | Player;
+  export type Player = "red" | "yellow";
+  export type Cell = "open" | Player;
   export type Column = Cell[];
   export type Board = Column[];
-  export type Game = {
+  export type GameState = {
+    status: "playing" | "draw" | "winner-red" | "winner-yellow";
     board: Board;
-    currentPlayer: Player
-  }
+    currentPlayer: Player;
+  };
 }
 
 const makeColumn = (length: number = BOARD_HEIGHT): State.Column =>
-  Array.from({ length }, (_v, _k) => 'open');
+  Array.from({ length }, (_v, _k) => "open");
 
 const makeBoard = (width: number = BOARD_WIDTH): State.Board =>
-  Array.from({length: width}, (_v, _k) => makeColumn());
+  Array.from({ length: width }, (_v, _k) => makeColumn());
 
 interface CellProps {
   cell: State.Cell;
 }
 
 const Cell: React.FC<CellProps> = (props: CellProps) => {
-  return (
-    <div className={`cell cell-state-${props.cell}`}></div>
-  )
-}
+  return <div className={`cell cell-state-${props.cell}`}></div>;
+};
 
 interface ColumnProps {
   columnId: number;
@@ -39,69 +38,80 @@ interface ColumnProps {
 const Column: React.FC<ColumnProps> = (props: ColumnProps) => {
   return (
     <div className="column" onClick={() => props.onClick(props.columnId)}>
-      {props.column.map((cell: State.Cell, idx: number) => <Cell cell={cell} key={`cell-${idx}`} />)}
+      {props.column.map((cell: State.Cell, idx: number) => (
+        <Cell cell={cell} key={`cell-${idx}`} />
+      ))}
     </div>
-  )
-}
-const initialState = (): State.Game => {
+  );
+};
+const initialState = (): State.GameState => {
   return {
-    currentPlayer: 'yellow',
+    status: "playing",
+    currentPlayer: "yellow",
     board: makeBoard(),
-  }
-}
+  };
+};
 
 const oppositePlayer = (player: State.Player): State.Player =>
-  player === 'red' ? 'yellow' : 'red';
+  player === "red" ? "yellow" : "red";
 
 const replace = <T,>(list: T[], idx: number, newVal: T): T[] => {
-  return [
-      ...list.slice(0, idx),
-      newVal,
-      ...list.slice(idx + 1),
-  ]
-}
+  return [...list.slice(0, idx), newVal, ...list.slice(idx + 1)];
+};
 // Note: Might want to switch to UseReducer instead
 // TODO: win conditions
 // TODO: unit tests (prolly easier with use reducer)
 
-function useGameState(){
+function useGameState() {
   const [state, setGameState] = useState(initialState());
 
   const playColumn = (columnId: number) => {
-    // find open spot
-    const openIndex = state.board[columnId]
-      .findLastIndex((cell: State.Cell) => cell === 'open')
-    if (openIndex < 0) return;
-
+    // find column
     const column = state.board.at(columnId);
     if (column === undefined) return;
 
+    // find open spot in column
+    const openIndex = column.findLastIndex(
+      (cell: State.Cell) => cell === "open"
+    );
+    if (openIndex < 0) return;
+
     const newColumn = replace(column, openIndex, state.currentPlayer);
-    const newBoard = replace(state.board, columnId, newColumn)
+    const newBoard = replace(state.board, columnId, newColumn);
 
     setGameState({
       ...state,
       currentPlayer: oppositePlayer(state.currentPlayer),
       board: newBoard,
     });
-  }
+  };
 
   return { ...state, playColumn } as const;
 }
 
 export default function App() {
-  const {currentPlayer, board, playColumn} = useGameState();
+  const { currentPlayer, board, playColumn } = useGameState();
 
-  const clickColumn = useCallback((idx: number) => {
-    playColumn(idx);
-  }, [board, currentPlayer, playColumn]);
+  const clickColumn = useCallback(
+    (idx: number) => {
+      playColumn(idx);
+    },
+    [board, currentPlayer, playColumn]
+  );
 
   return (
     <div className="App">
       <p>current player: {currentPlayer}</p>
       <div className={`board current-player-${currentPlayer}`}>
-        {board.map((column: State.Column, idx: number) => <Column column={ column } onClick={clickColumn} columnId={idx} key={`column-${idx}`}/>)}
+        {board.map((column: State.Column, idx: number) => (
+          <Column
+            column={column}
+            onClick={clickColumn}
+            columnId={idx}
+            key={`column-${idx}`}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
