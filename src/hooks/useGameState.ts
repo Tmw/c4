6f8a24b99972ed1677a76bd06905b88ea27a28e4
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2022-12-14 21:04:20 +0100

    Switch to useReducer internally instead

--- src/hooks/useGameState.ts
+++ src/hooks/useGameState.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import type { State } from "@/types";
 import { makeBoard, checkBoardState, putCell } from "@/utils/board-utils";
 
@@ -13,21 +13,56 @@ const initialState = (): State.GameState => {
 const oppositePlayer = (player: State.Player): State.Player =>
   player === "red" ? "yellow" : "red";
 
+type MakeAction<N extends string, P extends object> = {
+  type: N;
+  payload: P;
+};
+
+type SetCellAction = MakeAction<
+  "SetCellAction",
+  { columnId: number; player: State.Player }
+>;
+type ResetGameAction = MakeAction<"ResetGameAction", {}>;
+type Action = SetCellAction | ResetGameAction;
+
+function reducer(state: State.GameState, action: Action): State.GameState {
+  switch (action.type) {
+    case "SetCellAction": {
+      const newBoard = putCell(
+        state.board,
+        action.payload.columnId,
+        state.currentPlayer
+      );
+
+      return {
+        ...state,
+
+        currentPlayer: oppositePlayer(state.currentPlayer),
+        board: newBoard,
+        status: checkBoardState(newBoard),
+      };
+    }
+
+    case "ResetGameAction": {
+      return initialState();
+    }
+  }
+}
+
 export function useGameState() {
-  const [state, setGameState] = useState(initialState());
+  const [state, dispatch] = useReducer(reducer, initialState());
 
   const playColumn = (columnId: number) => {
-    const newBoard = putCell(state.board, columnId, state.currentPlayer);
-
-    setGameState({
-      ...state,
-      currentPlayer: oppositePlayer(state.currentPlayer),
-      board: newBoard,
-      status: checkBoardState(newBoard),
+    dispatch({
+      type: "SetCellAction",
+      payload: {
+        columnId: columnId,
+        player: state.currentPlayer,
+      },
     });
   };
 
-  const restartGame = () => setGameState(initialState());
+  const restartGame = () => dispatch({ type: "ResetGameAction", payload: {} });
 
   return { ...state, playColumn, restartGame } as const;
 }
